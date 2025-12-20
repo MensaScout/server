155e0eab98851a5f4a33316c4b48c8514f3337aa
official-Cromatin <lars-winzer@web.de>
2025-12-20 11:02:12 +0100

    Add: ValueObjects for Domain

---
+++ Sources/Domain/ValueObjects/Address.swift
@@ -0,0 +1,13 @@
+//
+//  Address.swift
+//  server
+//
+//  Created by Lars Winzer on 20.12.25.
+//
+
+struct Address {
+    let street: String
+    let postalCode: String
+    let city: String
+    let country: String
+}
---
+++ Sources/Domain/ValueObjects/Weekday.swift
@@ -0,0 +1,14 @@
+//
+//  Weekday.swift
+//  server
+//
+//  Created by Lars Winzer on 20.12.25.
+//
+
+enum Weekday: Int, CaseIterable {
+    case monday = 1
+    case tuesday
+    case wednesday
+    case thursday
+    case friday
+}
